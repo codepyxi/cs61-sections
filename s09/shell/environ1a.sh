933146a26bea9c13317ded49feac5b8fcda9413a
PYTHON GOLDSTEIN <pythongoldstein@MARKs-MacBook-Air-7.local>
2016-11-07 16:56:28 -0500

    Code ready for section

--- s09/shell/environ1a.sh
+++ s09/shell/environ1a.sh
@@ -1,4 +1,4 @@
 CLASS=cs61
 SCRIPT=environ2.sh
 echo "I <3" $CLASS
-./$SCRIPT
+sh $SCRIPT
--- s09/solutions/runafter_sol.c
+++ s09/solutions/runafter_sol.c
@@ -6,7 +6,11 @@
 #include <sys/stat.h>
 
 int main( int argc, char **argv ) {
-  (void)argc;
-  sleep( atoi(argv[1]) );
-  execvp(argv[2], &argv[2]);
+    
+    if(argc < 3){
+        printf("USAGE: ./runafter_sol TIME PROGRAM ARGS\n");
+        abort();
+    }
+    sleep( atoi(argv[1]) );
+    execvp(argv[2], &argv[2]);
 }
